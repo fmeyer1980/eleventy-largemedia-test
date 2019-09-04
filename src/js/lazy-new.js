44e59336f1e7c33e153355b28412969a079fd7bd
Flemming Meyer <fm@indexed.dk>
2019-09-05 01:21:03 +0200

    fdsfdsf

--- src/js/lazy-new.js
+++ src/js/lazy-new.js
@@ -1,32 +1,33 @@
-function lazyLoad(elements) {
+// function lazyLoad(elements) {
 
-    elements.forEach(picture => {
-      if (picture.intersectionRatio > 0) {
+//     elements.forEach(picture => {
+//       if (picture.intersectionRatio > 0) {
   
-        // gather all the image and source elements in this picture
-        var sources = picture.children;
+//         // gather all the image and source elements in this picture
+//         var sources = picture.children;
   
-        for (var s = 0; s < sources.length; s++) {
-          var source = sources[s];
+//         for (var s = 0; s < sources.length; s++) {
+//           var source = sources[s];
   
-          // set a new srcset on the source elements 
-          if (sources.hasAttribute("srcset")) {
-            source.setAttribute("srcset", ONE_OF_OUR_BIGGER_IMAGES);
-          }
-          // or a new src on the img element
-          else {
-            source.setAttribute("src", ONE_OF_OUR_BIGGER_IMAGES);
-          }
-        }
+//           // set a new srcset on the source elements 
+//           if (sources.hasAttribute("srcset")) {
+//             source.setAttribute("srcset", ONE_OF_OUR_BIGGER_IMAGES);
+//           }
+//           // or a new src on the img element
+//           else {
+//             source.setAttribute("src", ONE_OF_OUR_BIGGER_IMAGES);
+//           }
+//         }
   
-        // stop observing this element. Our work here is done!
-        observer.unobserve(item.target);
-      };
-    });
-  
-  };
+//         // stop observing this element. Our work here is done!
+//         observer.unobserve(item.target);
+//       };
+//     });
 
-  // remove the lazy class when the full image is loaded to unblur
-source.addEventListener('load', image => {
-    image.target.closest("picture").classList.remove("lazy")
-}, false);
+//       // remove the lazy class when the full image is loaded to unblur
+// source.addEventListener('load', image => {
+//     image.target.closest("picture").classList.remove("lazy")
+// }, false);
+  
+//   };
+
