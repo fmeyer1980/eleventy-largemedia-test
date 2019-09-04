9f1010490547a83c460df0b5c80cb963239369d5
Flemming Meyer <fm@indexed.dk>
2019-09-04 23:00:18 +0200

    dsfhghg

---
+++ src/js/lazy-new.js
@@ -0,0 +1,32 @@
+function lazyLoad(elements) {
+
+    elements.forEach(picture => {
+      if (picture.intersectionRatio > 0) {
+  
+        // gather all the image and source elements in this picture
+        var sources = picture.children;
+  
+        for (var s = 0; s < sources.length; s++) {
+          var source = sources[s];
+  
+          // set a new srcset on the source elements 
+          if (sources.hasAttribute("srcset")) {
+            source.setAttribute("srcset", ONE_OF_OUR_BIGGER_IMAGES);
+          }
+          // or a new src on the img element
+          else {
+            source.setAttribute("src", ONE_OF_OUR_BIGGER_IMAGES);
+          }
+        }
+  
+        // stop observing this element. Our work here is done!
+        observer.unobserve(item.target);
+      };
+    });
+  
+  };
+
+  // remove the lazy class when the full image is loaded to unblur
+source.addEventListener('load', image => {
+    image.target.closest("picture").classList.remove("lazy")
+}, false);
