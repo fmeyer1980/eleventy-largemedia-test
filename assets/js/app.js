bbc76a37fd193b9b3d05a4a178f771ed2fd557fc
Flemming Meyer <fm@indexed.dk>
2019-09-10 17:40:14 +0200

    new slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 import Swup from 'swup';
 import SwupScrollPlugin from '@swup/scroll-plugin';
-import Glide from '@glidejs/glide'
+import SwupPreloadPlugin from '@swup/preload-plugin';
+import Glide from '@glidejs/glide';
 
 // enable swup
 const swup = new Swup({
@@ -11,9 +12,20 @@ const swup = new Swup({
             scrollFriction: 0.3,
             scrollAcceleration: 0.04,
         }),
+        new SwupPreloadPlugin()
     ]
     
-  });
+});
+swup.cache.getCurrentPage();
+swup.cache.exists('/docs'); 
+swup.cache.cacheUrl({
+    title: 'Page title',    
+    pageClass: 'body-class',
+    originalContent: 'html content of page',
+    blocks: ['<div id="swup"></div>'], 
+    responseURL: '/redirected-url'
+});
+
 
 new Glide('.glide').mount()
 AOS.init();
