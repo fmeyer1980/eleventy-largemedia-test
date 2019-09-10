3adea036b3a782e73a01d2e6d70bbfdd44504ed9
Flemming Meyer <fm@indexed.dk>
2019-09-11 00:44:16 +0200

    dsafsdfds

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,57 +19,6 @@ const swup = new Swup({
     ]
     
 });
-swup.cache.cacheUrl({
-    title: 'Page title',    
-    pageClass: 'body-class',
-    originalContent: 'html content of page',
-    blocks: ['<div id="swup"></div>'], 
-    responseURL: '/redirected-url'
-});
-swup.cache.getCurrentPage();
-swup.cache.getPage('/docs');
-swup.cache.exists('/docs');
-swup.cache.remove('/docs');
-swup.cache.empty();
-
-
-// export class Cache {
-// 	constructor() {
-// 		this.pages = {};
-// 		this.last = null;
-// 	}
-
-// 	cacheUrl(page) {
-// 		if (page.url in this.pages === false) {
-// 			this.pages[page.url] = page;
-// 		}
-// 		this.last = this.pages[page.url];
-// 		this.swup.log(`Cache (${Object.keys(this.pages).length})`, this.pages);
-// 	}
-
-// 	getPage(url) {
-// 		return this.pages[url];
-// 	}
-
-// 	getCurrentPage() {
-// 		return this.getPage(window.location.pathname + window.location.search);
-// 	}
-
-// 	exists(url) {
-// 		return url in this.pages;
-// 	}
-
-// 	empty() {
-// 		this.pages = {};
-// 		this.last = null;
-// 		this.swup.log('Cache cleared');
-// 	}
-
-// 	remove(url) {
-// 		delete this.pages[url];
-// 	}
-// }
-// export default Cache;
 
 new Glide('.glide').mount()
 AOS.init();
